09bcb76f8fc679c0c9face8bbafa8eb37630c02d
KGRANGER <kevin.granger@ynov.com>
2022-02-06 22:42:09 +0100

    changed tutorial

--- TETRIS/TETRIS/Form1.Designer.cs
+++ TETRIS/TETRIS/Form1.Designer.cs
@@ -38,26 +38,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Ebrima", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Location = new System.Drawing.Point(15, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.Size = new System.Drawing.Size(254, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Comment jouer au Tetrics ?";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Location = new System.Drawing.Point(16, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(401, 403);
+            this.label2.Size = new System.Drawing.Size(498, 480);
             this.label2.TabIndex = 1;
             this.label2.Text = resources.GetString("label2.Text");
             // 
             // quit_1
             // 
-            this.quit_1.Location = new System.Drawing.Point(12, 471);
+            this.quit_1.Location = new System.Drawing.Point(16, 580);
+            this.quit_1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quit_1.Name = "quit_1";
-            this.quit_1.Size = new System.Drawing.Size(401, 23);
+            this.quit_1.Size = new System.Drawing.Size(535, 28);
             this.quit_1.TabIndex = 2;
             this.quit_1.Text = "Quitter";
             this.quit_1.UseVisualStyleBackColor = true;
@@ -65,14 +68,15 @@
             // 
             // Help
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 500);
+            this.ClientSize = new System.Drawing.Size(564, 615);
             this.Controls.Add(this.quit_1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Help";
             this.Text = "Aide pour jouer";
